fb0cb624517e96c242f84a8eb5508f78dab9a1af
cuigw <562863063@qq.com>
2017-10-17 22:07:30 +0800

    vcs refresh file status

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,40 @@
 public class Test {
    private String name="ssss";
+   private String names="ss3ss";
+   private String name2="ssss";
+   private String pass="SSDF";
 
+   public String getName() {
+      return name;
+   }
 
+   public void setName(String name) {
+      this.name = name;
+   }
 
+   public String getNames() {
+      return names;
+   }
 
+   public void setNames(String names) {
+      this.names = names;
+   }
 
+   public String getName2() {
+      return name2;
+   }
+
+   public void setName2(String name2) {
+      this.name2 = name2;
+   }
+
+   public String getPass() {
+      return pass;
+   }
+
+   public void setPass(String pass) {
+      this.pass = pass;
+   }
 }
+
+
